35be9a969db2a495ac0a33060d0584d3ee355a84
Adamowicz <r.adamowicz4@wp.pl>
2019-05-18 13:00:16 +0200

    Usuniecie autoloadera, poprawa gitignore

--- application/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function autoload($className)
-{
-    $className = LIB . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
-    require $className;
-}
-
-spl_autoload_register('autoload');

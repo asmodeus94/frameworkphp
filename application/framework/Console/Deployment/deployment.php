d7916791216957c86083ba211332312252c319c7
Adamowicz <r.adamowicz4@wp.pl>
2019-06-04 20:47:21 +0200

    Rozbudowa skryptu od deploymentu

--- application/framework/Console/Deployment/deployment.php
+++ application/framework/Console/Deployment/deployment.php
@@ -3,6 +3,12 @@ if (php_sapi_name() !== 'cli') {
     exit;
 }
 
-echo __FILE__;
-//$filesystem = new \Symfony\Component\Filesystem\Filesystem();
-//$filesystem->remove($files);
+define('APP', '.' . DIRECTORY_SEPARATOR . 'application' . DIRECTORY_SEPARATOR);
+define('VENDOR', APP . 'vendor' . DIRECTORY_SEPARATOR);
+define('CACHE_AUTOWIRING', APP . 'cache' . DIRECTORY_SEPARATOR . 'autowiring' . DIRECTORY_SEPARATOR);
+
+require VENDOR . 'autoload.php';
+
+$filesystem = new \Symfony\Component\Filesystem\Filesystem();
+$filesystem->remove(CACHE_AUTOWIRING);
+$filesystem->mkdir(CACHE_AUTOWIRING);
--- public/index.php
+++ public/index.php
@@ -1,7 +1,6 @@
 <?php
 define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 define('APP', ROOT . 'application' . DIRECTORY_SEPARATOR);
-define('LIB', APP . 'lib' . DIRECTORY_SEPARATOR);
 define('VENDOR', APP . 'vendor' . DIRECTORY_SEPARATOR);
 
 require VENDOR . 'autoload.php';
